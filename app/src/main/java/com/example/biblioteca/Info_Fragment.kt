b96e525ac56bd730678df08e36d98dbfa81e2f2f
Joseph <joseph.mangiapane@community.unipa.it>
2023-05-27 17:24:56 +0200

    gg

--- app/src/main/java/com/example/biblioteca/Info_Fragment.kt
+++ app/src/main/java/com/example/biblioteca/Info_Fragment.kt
@@ -1,4 +1,22 @@
 package com.example.biblioteca
 
-class Info_Fragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.biblioteca.databinding.InfoLayoutBinding
+
+class Info_Fragment : Fragment() {
+
+    private lateinit var binding: InfoLayoutBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = InfoLayoutBinding.inflate(inflater)
+
+        return binding.root
+    }
 }
--- app/src/main/java/com/example/biblioteca/MainActivity.kt
+++ app/src/main/java/com/example/biblioteca/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.biblioteca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.biblioteca.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val home_binding = binding.tastoHome
+        home_binding.setOnClickListener{
+            val fragmentManager = supportFragmentManager
+            val transaction = fragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentMain,Home_Fragment())
+            transaction.commit()
+        }
+
+        val info_binding = binding.tastoInfo
+        info_binding.setOnClickListener{
+            val fragmentManager = supportFragmentManager
+            val transaction = fragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentMain,Info_Fragment())
+            transaction.commit()
+        }
     }
+
+
+
+
 
 }
