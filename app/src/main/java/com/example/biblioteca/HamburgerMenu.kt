6f40bac10534b09600ad958b934cc571ece16717
guido <guido.lamia@community.unipa.it>
2023-06-05 17:48:27 +0200

    k k

---
+++ app/src/main/java/com/example/biblioteca/HamburgerMenu.kt
@@ -0,0 +1,19 @@
+package com.example.biblioteca
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.biblioteca.databinding.LibroLayoutBinding
+
+class HamburgerMenu:Fragment() {
+    private lateinit var binding: LibroLayoutBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+}
